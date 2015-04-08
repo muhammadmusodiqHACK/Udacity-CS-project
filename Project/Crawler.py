378ec4300fabe66aa8bdcebd911bf6305fcdd164
Raphael Junior <raphnano@gmail.com>
2015-04-08 15:10:48 +0200

    A start to function definitions

---
+++ Project/Crawler.py
@@ -0,0 +1,9 @@
+__author__ = 'Raphael'
+
+def getLink(page):
+    """
+    function takes in a html  string and returns first link
+    :param page: html string
+    :return: first link
+    """
+    return page
